0f05b8a2ce2532f3c173c5f47c054888488ec45b
BuildTools <unconfigured@null.spigotmc.org>
2021-04-22 14:13:01 +0500

    Task1Reduction тоже работает

--- lab4/Source.cpp
+++ lab4/Source.cpp
@@ -4,41 +4,37 @@
 #include <ctime>
 
 void Task1ParallelFor(double** arr, long long N, int k, double& avg) {
-	//for(long long ij=0;ij<N*N;++ij)
-	long long i, j;
 	double sum;
-#pragma omp parallel for private (i,j,sum)
-		for(i = 0; i < N; ++i) {
-			sum = 0;
-			for ( j = 0; j < N; ++j) {
-				arr[i][j] = sin(i) + cos(j);
-				sum += arr[i][j];
-			}
-			avg += sum;
+#pragma omp parallel for num_threads(k)
+	for (long long i = 0; i < N; ++i) {
+		double sum = 0;
+		for (long long j = 0; j < N; ++j) {
+			arr[i][j] = sin(i) + cos(j);
+			sum += arr[i][j];
 		}
-	
+		avg += sum;
+	}
 	avg /= (N * N);
 }
 //
-//void Task1Reduction(double** arr, long long N, int k, double& avg) {
-//	double localAvg = 0;
-//#pragma omp parallel num_threads(k) reduction(+:localAvg)
-//	{
-//		int currThread = omp_get_thread_num();
-//		for (long long i = currThread*N/k; i < (currThread+1)*N/k; ++i) {
-//			for (long long j = currThread * N / k; j < (currThread + 1) * N / k; ++j) {
-//				arr[i][j] = sin(i) + cos(j);
-//				localAvg += arr[i][j];
-//			}
-//		}
-//	}
-//	localAvg /= N * N;
-//	avg = localAvg;
-//}
+void Task1Reduction(double** arr, long long N, int k, double& avg) {
+	double sum = 0;
+#pragma omp parallel num_threads(k) reduction(+:sum)
+	{
+		int currThread = omp_get_thread_num();
+		for (long long i = currThread*N/k; i < (currThread+1)*N/k; ++i) {
+			for (long long j = 0; j <  N; ++j) {
+				arr[i][j] = sin(i) + cos(j);
+				sum += arr[i][j];
+			}
+		}
+	}
+	avg = sum/(N*N);
+}
 
 int main() {
 	const long long N = 10000;
-	double avg = 0;
+	double serialAvg = 0;
 	clock_t start, end;
 	printf("%lld\n", N);
 	double** arr = new double* [N];
@@ -48,26 +44,26 @@ int main() {
 	for (long long i = 0; i < N; ++i)
 		for (long long j = 0; j < N; ++j) {
 			arr[i][j] = sin(i) + cos(j);
-			avg += arr[i][j];
+			serialAvg += arr[i][j];
 		}
-	avg /= N * N;
+	serialAvg /= N * N;
 	end = clock();
-	printf("SERIAL value: %f, time:%f\n", avg, (double)end - start / CLOCKS_PER_SEC);
+	printf("SERIAL value: %f, time:%f\n", serialAvg, (double)(end - start) / CLOCKS_PER_SEC);
 
 	const int arrThreadSize = 3;
 	int arrThreads[arrThreadSize] = { 2,4,8 };
 	for (int k = 0; k < arrThreadSize; ++k)
 	{
-		avg = 0;
-		start = clock();
-		Task1ParallelFor(arr, N, arrThreads[k], avg);
-		end = clock();
-		printf("Task1ParallelFor value: %f, time: %f, threads: %d\n", avg, (double)end - start / CLOCKS_PER_SEC, arrThreads[k]);
-		//avg = 0;
+		double parallelAvg = 0;
 		//start = clock();
-		//Task1Reduction(arr, N, arrThreads[k], avg);
+		//Task1ParallelFor(arr, N, arrThreads[k], parallelAvg);
 		//end = clock();
-		//printf("Task1Reduction value: %f, time: %f, threads: %d\n", avg, (double)end - start / CLOCKS_PER_SEC, arrThreads[k]);
+		//printf("Task1ParallelFor value: %f, time: %f, threads: %d\n", parallelAvg, (double)(end - start) / CLOCKS_PER_SEC, arrThreads[k]);
+		parallelAvg = 0;
+		start = clock();
+		Task1Reduction(arr, N, arrThreads[k], parallelAvg);
+		end = clock();
+		printf("Task1Reduction value: %f, time: %f, threads: %d\n", parallelAvg, (double)(end - start) / CLOCKS_PER_SEC, arrThreads[k]);
 	}
 	return 0;
 }
