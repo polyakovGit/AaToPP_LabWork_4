172b6758141583f1b7192a6f5a95d411e307dbf9
BuildTools <unconfigured@null.spigotmc.org>
2021-04-22 13:49:36 +0500

    я не уверен что это должно работать так в функции Task1ParallelFor

--- lab4/Source.cpp
+++ lab4/Source.cpp
@@ -4,21 +4,40 @@
 #include <ctime>
 
 void Task1ParallelFor(double** arr, long long N, int k, double& avg) {
-#pragma omp parallel num_threads(k)
-	{
-#pragma omp for
-		for (long long i = 0; i < N; ++i) {
-			for (long long j = 0; j < N; ++j) {
+	//for(long long ij=0;ij<N*N;++ij)
+	long long i, j;
+	double sum;
+#pragma omp parallel for private (i,j,sum)
+		for(i = 0; i < N; ++i) {
+			sum = 0;
+			for ( j = 0; j < N; ++j) {
 				arr[i][j] = sin(i) + cos(j);
-				avg += arr[i][j];
+				sum += arr[i][j];
 			}
+			avg += sum;
 		}
-	}
-avg /= N*N;
+	
+	avg /= (N * N);
 }
+//
+//void Task1Reduction(double** arr, long long N, int k, double& avg) {
+//	double localAvg = 0;
+//#pragma omp parallel num_threads(k) reduction(+:localAvg)
+//	{
+//		int currThread = omp_get_thread_num();
+//		for (long long i = currThread*N/k; i < (currThread+1)*N/k; ++i) {
+//			for (long long j = currThread * N / k; j < (currThread + 1) * N / k; ++j) {
+//				arr[i][j] = sin(i) + cos(j);
+//				localAvg += arr[i][j];
+//			}
+//		}
+//	}
+//	localAvg /= N * N;
+//	avg = localAvg;
+//}
 
 int main() {
-	const long long N = 20;
+	const long long N = 10000;
 	double avg = 0;
 	clock_t start, end;
 	printf("%lld\n", N);
@@ -31,7 +50,7 @@ int main() {
 			arr[i][j] = sin(i) + cos(j);
 			avg += arr[i][j];
 		}
-	avg /= N*N;
+	avg /= N * N;
 	end = clock();
 	printf("SERIAL value: %f, time:%f\n", avg, (double)end - start / CLOCKS_PER_SEC);
 
@@ -44,6 +63,11 @@ int main() {
 		Task1ParallelFor(arr, N, arrThreads[k], avg);
 		end = clock();
 		printf("Task1ParallelFor value: %f, time: %f, threads: %d\n", avg, (double)end - start / CLOCKS_PER_SEC, arrThreads[k]);
+		//avg = 0;
+		//start = clock();
+		//Task1Reduction(arr, N, arrThreads[k], avg);
+		//end = clock();
+		//printf("Task1Reduction value: %f, time: %f, threads: %d\n", avg, (double)end - start / CLOCKS_PER_SEC, arrThreads[k]);
 	}
 	return 0;
 }
